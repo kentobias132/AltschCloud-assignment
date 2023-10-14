a52119668081744647e8bb8a48ca7b07b78971ad
kentobia132 <thobhi2@gmail.com>
2023-10-14 14:14:18 +0100

    Fifteen (15) file operators in linux with explanation

---
+++ assignment2.sh
@@ -0,0 +1,48 @@
+
+##  With explanation, write out fifteen (15) file operators 
+
+1. `ls` - List Files:
+   - This command lists files and directories in the current directory or a specified directory.
+
+2. `cat` - Concatenate Files:
+   - It is used to display the contents of one or more files or concatenate their contents.
+
+3. `touch` - Create Empty Files:
+   - This command is used to create new, empty files. If the file already exists, it updates the file's timestamp.
+
+4. `cp` - Copy Files:
+   - Copies files or directories from one location to another.
+
+5. `mv` - Move/Rename Files:
+   - Moves files or directories to a different location or renames them.
+
+6. `rm` - Remove Files:
+   - Deletes files or directories. Be cautious as this operation is irreversible.
+
+7. `mkdir` - Create Directories:
+   - Creates new directories or folders.
+
+8. `rmdir` - Remove Directories:
+   - Deletes empty directories.
+
+9. `find` - Search for Files:
+   - Searches for files and directories in a specified location based on various criteria like name, size, or type.
+
+10. `grep` - Search Text in Files:
+    - Searches for specific text patterns within one or more files.
+
+11. `chmod` - Change File Permissions:
+    - Modifies file permissions, specifying who can read, write, or execute a file.
+
+12. `chown` - Change File Ownership:
+    - Changes the owner and group of a file or directory.
+
+13. `ln` - Create Links:
+    - Creates hard links or symbolic links to files. Links allow multiple references to the same file.
+
+14. `stat` - Display File Status:
+    - Provides detailed information about a file, including its size, timestamps, and permissions.
+
+15. `file` - Determine File Type:
+    - Identifies the type of a file, such as whether it's a text file, binary file, or some other format.
+
